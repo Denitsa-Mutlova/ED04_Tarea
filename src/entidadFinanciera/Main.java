90109523cf03dfaa7da94f2235efc45312e96cfc
Denitsa Mutlova <dmutlova@gmail.com>
2024-03-10 19:23:38 +0100

    REFACTORIZACIÓN

--- src/entidadFinanciera/Main.java
+++ src/entidadFinanciera/Main.java
@@ -2,21 +2,39 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
-package tarea4endes24;
-
+package entidadFinanciera;
 
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta objetoCuenta;
         double saldoActual;
-        
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        objetoCuenta = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = objetoCuenta.estado();
+        System.out.println("El saldo actual es" + saldoActual);
         
-    
+        probarRetirar(objetoCuenta,3000, 0.0);
+        probarIngresar(objetoCuenta,-3000, 0.0);
+        
     }
+    
+    public static void probarRetirar(CCuenta cuenta, double cantidad, double cantidadEsperada) {
+    try {
+        cuenta.retirar(cantidad);
+        System.out.println("Retirar con éxito. Nuevo saldo: " + cuenta.estado());
+    } catch (Exception e) {
+        System.err.println("Error al retirar: " + e.getMessage());
+    }
+}
+    
+    public static void probarIngresar(CCuenta cuenta, double cantidad, double cantidadEsperada) {
+    try {
+        cuenta.ingresar(cantidad);
+        System.out.println("Ingresar con éxito. Nuevo saldo: " + cuenta.estado());
+    } catch (Exception e) {
+        System.err.println("Error al ingresar: " + e.getMessage());
+    }
+}
+    
 }
